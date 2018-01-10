76870b241601e15f6acfaf8cfe4acdcbb83f74f1
paulogonzalez5679 <33768253+paulogonzalez5679@users.noreply.github.com>
2018-01-10 16:52:41 -0500

    PROYECTO CREADO, AGREGADA LA LIBRERIA Y LA CLASE PRINCIPAL, CREADO EL PAQUETE PARA CREAR LAS CLASES DE PROPIETARIO Y CASA

---
+++ Ejercicio_03_02/src/ejercicio_03_02/Ejercicio_03_02.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_03_02;
+
+import Librerias.Diccionario;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author paulo
+ */
+public class Ejercicio_03_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner lectura = new Scanner(System.in);
+        Map<Integer,String>mapa= new HashMap<>();
+        Diccionario d = new Diccionario(mapa);
+        
+        System.out.println("Cuantos usuarios ingresara");
+        int n= lectura.nextInt();
+        lectura.nextLine();
+        
+        for( int i= 0; i<n;i++)
+        {
+            System.out.println("Ingrese Usuario: ");
+            String n2= lectura.nextLine();
+            d.agregarElemento(i,n2);
+        }
+        Object elemento=d.recuperarELemento(1);
+        System.out.print(elemento);
+        d.eliminarElemento(elemento);
+    }
+    
+}
